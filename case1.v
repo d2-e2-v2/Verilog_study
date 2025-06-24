ba2ac09ecf1468f39cce57e23ae94c69e575e961
d2-e2-v2 <iec2024108@iiita.ac.in>
2025-06-24 21:18:34 +0530

    Update case1.v

--- case1.v
+++ case1.v
@@ -1,37 +1,16 @@
 `timescale 1ps / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.06.2025 15:14:59
-// Design Name: 
-// Module Name: case1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 
-
-
-
+//  A basic strucrutal jk flip flop
 module jkflipflop(q,qbar,j,k,clock,clear); // this will use nand.
 input  j,k,clock,clear;
 output wire q,qbar;
 wire intq,intq2;
    
 
-nand_3 k1222(.out(intq),.in1(j),.in2(clock),.in3(qbar));
-nand_3 K222(.out(intq2),.in1(k),.in2(clock),.in3(q));
- nand_3 j1( q,intq,qbar,clear);
- nand_3 j2(qbar,intq2,q,clear);
-
+nand k1222(.out(intq),.in1(j),.in2(clock),.in3(qbar));
+nand K222(.out(intq2),.in1(k),.in2(clock),.in3(q));
+nand j1( q,intq,qbar,clear);
+nand j2(qbar,intq2,q,clear);
 
 endmodule
 
