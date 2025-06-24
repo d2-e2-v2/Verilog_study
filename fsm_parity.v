f046d0e9c820e8814208e248d3d56e4c0bbe6f8d
d2-e2-v2 <iec2024108@iiita.ac.in>
2025-06-24 21:21:28 +0530

    Update fsm_parity.v

--- fsm_parity.v
+++ fsm_parity.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.06.2025 13:51:18
-// Design Name: 
-// Module Name: fsm_parity
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module fsm_parity (in,out,clock );
 input clock,in;  
