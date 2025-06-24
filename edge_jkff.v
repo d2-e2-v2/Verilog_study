28ca6f49fd197d5aad55a40db1faee0d685f5624
d2-e2-v2 <iec2024108@iiita.ac.in>
2025-06-24 21:20:23 +0530

    Update edge_jkff.v

--- edge_jkff.v
+++ edge_jkff.v
@@ -1,31 +1,13 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12.06.2025 21:38:40
-// Design Name: 
-// Module Name: edge_jkff
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+// this flip-flop uses edge triggering using master slave method.
+// jkflipflop used is named jkff in the repo thanks.
 
 module ms_jkff( q,qbar,clock,clear,j,k);
 input clock,clear,j,k;
 output wire q,qbar;
 wire q1,q1bar;
 jkflipflop f1(.q(q1),.qbar(q1bar),.j(j),.k(k),.clock(clock),.clear(clear));
-sr l1(.clock(~clock),.q(q),.qbar(qbar),.set(q1),.reset(q1bar),.clear(clear));
+  sr l1(.clock(~clock),.q(q),.qbar(qbar),.set(q1),.reset(q1bar),.clear(clear)); // latch holds the value for the flipflop
 
 
 endmodule
