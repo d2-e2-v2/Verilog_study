b2be7a5eace61ef3db434c2b4ce623fd7ecdb00e
d2-e2-v2 <iec2024108@iiita.ac.in>
2025-06-24 21:13:11 +0530

    Update sr.v

--- sr.v
+++ sr.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.06.2025 09:54:54
-// Design Name: 
-// Module Name: sr
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module sr(
      set,
      reset,
@@ -36,6 +15,6 @@ module sr(
     wire sn,rn;
   nand n1(sn,set,clock);
   nand n2 (rn,reset,clock);
-  nand_3 k1(q,sn,clear,qbar);
-  nand_3 k2(qbar,rn,clear,q);
+  nand k1(q,sn,clear,qbar);
+  nand k2(qbar,rn,clear,q);
 endmodule
